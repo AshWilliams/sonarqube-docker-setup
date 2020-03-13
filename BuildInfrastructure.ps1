894495c5b6be8afe9b5975928753a89789774f0c
Andres Iturralde <andresi@microsoft.com>
2020-03-13 00:15:52 -0300

    COmpilacion y publicacion de contenedor Docker

--- BuildInfrastructure.ps1
+++ BuildInfrastructure.ps1
@@ -1,3 +1,10 @@
+$CONTAINER_IMAGE_NAME="sonarqube"
+$CONTAINER_IMAGE_TAG="latest"
+$CONTAINER_FULL_NAME = $CONTAINER_IMAGE_NAME + ":" + $CONTAINER_IMAGE_TAG
+$CONTAINER_REGISTRY_NAME="acrsonarqubeprod"
+$CONTAINER_REGISTRY_FQDN="$CONTAINER_REGISTRY_NAME.azurecr.io"
+$CONTAINER_FULL_NAME_WITH_FQDN=$CONTAINER_FULL_NAME+":"+$CONTAINER_REGISTRY_FQDN
+
 #Navega a ruta de CorePlatform (Resource Group, SQL Azure y Azure Container Registry) y los crea
 Set-Location .\CorePlatform
 
@@ -11,7 +18,16 @@ write-host $connectionString
 Set-Location -Path ".."
 
 
-#Publica imagen de Docker a ACR
+#Compilar imagen de docker y publicarla en el ACR
+Set-Location .\Docker
+
+az acr login --name $CONTAINER_REGISTRY_NAME
+
+docker build -t $CONTAINER_FULL_NAME .
+docker tag $CONTAINER_FULL_NAME $CONTAINER_FULL_NAME_WITH_FQDN
+docker push $CONTAINER_FULL_NAME_WITH_FQDN
+
+Set-Location -Path ".."
 
 #https://github.com/Hupka/sonarqube-azure-setup
 #https://pumpingco.de/blog/deploy-an-azure-web-app-for-containers-with-terraform/
